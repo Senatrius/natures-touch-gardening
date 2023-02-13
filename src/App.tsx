0a722c2da46a5f184f711ad8bf1ec6cab9237120
senatrius <g.makutenas@gmail.com>
2023-02-14 00:20:42 +0200

    add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Announcement } from './components/Announcement';
 import { Header } from './components/Header';
+import { Hero } from './components/Hero';
 
 function App() {
   return (
     <>
       <Announcement message='We’re sorry but due to COVID restrictions we are not providing services at this moment.' />
       <Header />
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,29 @@
+export const Hero = () => {
+  return (
+    <section className='mx-auto flex w-full max-w-[80rem] flex-col items-stretch md:w-[88%] md:flex-row-reverse md:justify-between lg:w-[83%]'>
+      <div className='block max-h-[20rem] overflow-hidden md:max-h-none md:min-h-full md:basis-5/12 lg:basis-1/2'>
+        <img
+          className='min-h-full object-cover object-bottom md:object-[center,10%]'
+          src='/hero.webp'
+          alt=''
+        />
+      </div>
+      <div className='max-w-[30rem] self-center py-16 text-center md:max-w-[33.75rem] md:basis-7/12 md:self-start md:py-24 md:pr-12 md:text-left lg:basis-1/2 lg:pr-16'>
+        <h1 className='mb-4 text-[1.875rem] font-semibold leading-[1.1] md:mb-6 md:text-[2.5rem] lg:text-[3.25rem]'>
+          Transforming Your Outdoor Space with Nature’s Touch
+        </h1>
+        <p className='mb-6 leading-snug text-body md:mb-10'>
+          We specialize in creating and maintaining beautiful, thriving gardens
+          that bring joy to your life. With our expert knowledge and passion for
+          gardening, we will help you transform your outdoor space into a lush,
+          vibrant oasis.
+        </p>
+        <a
+          className='inline-block rounded-[.25rem] bg-primary-100 py-6 px-9 text-[1.125rem] text-white hover:brightness-125 focus:brightness-125'
+          href='/'>
+          See our services
+        </a>
+      </div>
+    </section>
+  );
+};
