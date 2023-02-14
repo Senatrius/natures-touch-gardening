fecc6fa8be8bcfe5e351ac48628f90b06db091e0
senatrius <g.makutenas@gmail.com>
2023-02-14 15:04:29 +0200

    adjust spacing

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -2,14 +2,9 @@ import { SocialCounter } from './SocialCounter';
 
 export const Stats = () => {
   return (
-    <section className='relative mb-[3.5rem] w-full overflow-hidden bg-overlay md:mb-[6.25rem]'>
-      <img
-        className='absolute min-h-full w-full object-cover opacity-20'
-        src='/stats.webp'
-        alt=''
-      />
+    <section className='lg relative mb-[3.5rem] w-full overflow-hidden bg-overlay md:mb-[6.25rem]'>
       <h2 className='sr-only'>Social proof</h2>
-      <div className='mx-auto grid max-w-[65rem] grid-cols-2 gap-8 py-[3.5rem] md:grid-cols-4 md:py-[6.25rem]'>
+      <div className='mx-auto grid w-[93%] max-w-[65rem] grid-cols-1 gap-8 py-[3.5rem] sm:grid-cols-2 md:w-[88%] md:grid-cols-4 md:py-[6.25rem] lg:w-[83%]'>
         <SocialCounter
           value='275'
           label='Happy clients'
