cd07b1a1aae0f86574f2d57e313843a8ce35dbfe
senatrius <g.makutenas@gmail.com>
2023-02-15 00:51:47 +0200

    fix wcag error

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -2,7 +2,7 @@ import { SocialCounter } from './SocialCounter';
 
 export const Stats = () => {
   return (
-    <section className='lg relative mb-[3.5rem] w-full overflow-hidden bg-overlay md:mb-[6.25rem]'>
+    <section className='lg relative mb-[3.5rem] w-full overflow-hidden bg-primary-100 md:mb-[6.25rem]'>
       <h2 className='sr-only'>Social proof</h2>
       <div className='mx-auto grid w-[93%] max-w-[65rem] grid-cols-1 gap-8 py-[3.5rem] sm:grid-cols-2 md:w-[88%] md:grid-cols-4 md:py-[6.25rem] lg:w-[83%]'>
         <SocialCounter
